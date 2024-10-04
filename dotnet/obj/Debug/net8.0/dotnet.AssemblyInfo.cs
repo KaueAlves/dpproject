92adc0d582328b0adb8cb37e6cc02cef7e180023
kaue.alves <kaue.alves@castgroup.com.br>
2024-10-03 21:27:06 -0300

    Configurado Scaffold

--- dotnet/obj/Debug/net8.0/dotnet.AssemblyInfo.cs
+++ dotnet/obj/Debug/net8.0/dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ffd9fe1504b0d9183b987ea10a3d0ec21109e97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f30d864e40130fd33e54732895f85e2b87649d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
