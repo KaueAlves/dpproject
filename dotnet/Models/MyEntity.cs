5d3798e1b8696d0a99841d23d012a0b760a36e46
kaue.alves <kaue.alves@castgroup.com.br>
2024-10-04 22:02:06 -0300

    Retirando projeto dotnet

--- dotnet/Models/MyEntity.cs
+++
@@ -1,8 +0,0 @@
-namespace Dpproject.Data
-{
-    public class MyEntity
-    {
-        public int Id { get; set; }
-        public required string Name { get; set; } // Adicione as propriedades que você precisar
-    }
-}
